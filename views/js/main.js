e402a1b47a613aef9c1c51020a9294382b99a533
sahel mastoureshgh <sahel.mastoureshgh@gmail.com>
2014-11-20 19:43:12 -0500

    removed img creation from for loop

--- views/js/main.js
+++ views/js/main.js
@@ -531,12 +531,12 @@ document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   var movingPizza1=document.querySelector("#movingPizzas1");
+  var elem = document.createElement('img');
+  elem.className = 'mover';
+  elem.src = "images/pizza.png";
+  elem.style.height = "100px";
+  elem.style.width = "73.333px";
   for (var i = 0; i < 200; i++) {
-    var elem = document.createElement('img');
-    elem.className = 'mover';
-    elem.src = "images/pizza.png";
-    elem.style.height = "100px";
-    elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
     movingPizza1.appendChild(elem);
